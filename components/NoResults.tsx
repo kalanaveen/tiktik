6a779c59b8dd661d341b71f5376643d3a7d4e1cf
naveen kala <kalanaveen55@gmail.com>
2022-11-30 13:46:54 +0530

    videocard created

---
+++ components/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoResults = () => {
+  return (
+    <div>
+      noresults
+    </div>
+  )
+}
+
+export default NoResults
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,33 @@
+import axios from 'axios';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
+import { Video } from '../types';
 
-export default function Home() {
-  return (
-    <h1 className="text-4xl underline">Hello world!</h1>
-  );
+interface IProps {
+  videos: Video[];
 }
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos?.map((video: Video) => (
+          <VideoCard key={video._id} post={video} isShowingOnHome/>
+        ))
+      ) : (
+        <NoResults text={`No Vidoes`} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/post');
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+export default Home;
