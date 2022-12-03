7e0cd7887781f57478cbc3a3e007eeb51256b822
naveen kala <kalanaveen55@gmail.com>
2022-12-03 20:04:40 +0530

    upload video function added

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -27,7 +27,28 @@ const upload = () => {
     if (!userProfile) router.push('/');
   }, [userProfile, router]);
 
-  const uploadVideo = () => {};
+  const uploadVideo = async (e: any) => {
+    const selectedFile = e.target.files[0];
+    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg'];
+
+    // uploading asset to sanity 
+    if (fileTypes.includes(selectedFile.type)) {
+      setWrongFileType(false);
+      setLoading(true);
+
+      client.assets.upload('file', selectedFile, {
+        contentType: selectedFile.type,
+        filename: selectedFile.name,
+      })
+        .then((data) => {
+          setVideoAsset(data);
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+      setWrongFileType(true);
+    }
+  };
   const handlePost = () => {};
   const handleDiscard = () => {};
 
@@ -99,7 +120,55 @@ const upload = () => {
               </div>
             )}
           </div>
-          
+          {wrongFileType && (
+            <p className="text-center text-xl text-red-400 font-semibold mt-4 w-[260px]">
+              Please select an video file (mp4 or webm or ogg)
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col gap-3 pb-10">
+          <label className="text-md font-medium ">Caption</label>
+          <input
+            type="text"
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+            className="rounded lg:after:w-650 outline-none text-md border-gray-200 p-2"
+          />
+          <label className="text-md font-medium">Choose a topic</label>
+
+          <select
+            onChange={(e) => {
+              setTopic(e.target.value);
+            }}
+            className="outline-none lg:w-650 border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer"
+          >
+            {topics.map((item) => (
+              <option
+                value={item.name}
+                key={item.name}
+                className="outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+              >
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <div className="flex gap-6 mt-10">
+            <button
+              className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+              type="button"
+              onClick={handleDiscard}
+            >
+              Discard
+            </button>
+            <button
+              className="bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+              type="button"
+              onClick={handlePost}
+              disabled={videoAsset?.url ? false : true}
+            >
+              {savingPost ? 'Posting...' : 'Post'}
+            </button>
+          </div>
         </div>
       </div>
     </div>
